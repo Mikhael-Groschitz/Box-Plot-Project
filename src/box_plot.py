bf81c3b803d539f86ec9abe0ce20fc2e108b9885
Mika Hell <161988867+Mikhael-Groschitz@users.noreply.github.com>
2024-09-02 21:28:00 -0300

    Update box_plot.py

--- src/box_plot.py
+++ src/box_plot.py
@@ -2,22 +2,18 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# Carregar o dataset de despesas
+# Carregar o dataset de despesas que deseja (despesas_10/100/500/1000)
 df = pd.read_csv('../data/despesas_1000.csv')
 
-# Configurar o estilo dos gráficos
 sns.set(style="whitegrid")
 
-# Criar o box plot
 plt.figure(figsize=(12, 8))
 ax = sns.boxplot(x='loja', y='despesa', data=df, palette="Set2")
 
-# Adicionar título e labels
 plt.title('Distribuição das Despesas Mensais por Tipo de Loja', fontsize=16)
 plt.xlabel('Tipo de Loja', fontsize=14)
 plt.ylabel('Despesa (R$)', fontsize=14)
 
-# Adicionar etiquetas aos valores do box plot
 for i, artist in enumerate(ax.artists):
     artist.set_edgecolor('black')
     artist.set_linewidth(1.2)
@@ -28,7 +24,6 @@ for i, artist in enumerate(ax.artists):
             y = line.get_ydata()[1]
             ax.text(x, y, f'{y:.2f}', horizontalalignment='center', size=10, color='black', weight='semibold')
 
-# Ajustar o layout e exibir o gráfico
 plt.xticks(rotation=45)
 plt.tight_layout()
 plt.show()
